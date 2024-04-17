b9349ece36465687c2615b9ba487ea332728701f
nitindahiya199 <nitindahiya00000@gmail.com>
2024-04-17 23:05:49 +0530

    footer section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image"
 import HomeHeader from "../components/HomeHeader"
 import HomeSearch from "../components/HomeSearch"
+import Footer from "../components/Footer"
 
 const Home = () => {
   return (
-    <div className="">
+    <div className="flex flex-col justify-between min-h-screen">
       <HomeHeader />
-      <div className="flex flex-col items-center mt-24">
+      <div className="flex flex-col items-center ">
         <Image 
         src={`https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png`}
         alt="google logo"
@@ -17,6 +18,7 @@ const Home = () => {
         />
         <HomeSearch />
       </div>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/CountryLookup.jsx
@@ -0,0 +1,28 @@
+
+'use client'
+import { useEffect, useState } from "react";
+
+export default function CountryLookup() {
+    const [country, setCountry] = useState('India');
+
+    useEffect(() => {
+        const getCountry = async () => {
+            try {
+                const response = await fetch(`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`).then(res => res.json());
+                const countryData = response.country;
+                if (!countryData) return;
+                setCountry(countryData);
+                console.log(response.country);
+            } catch (error) {
+                console.error("Error fetching country:", error);
+            }
+        };
+        getCountry();
+    }, []);
+
+    return (
+        <div className="">
+            {country}
+        </div>
+    );
+}
